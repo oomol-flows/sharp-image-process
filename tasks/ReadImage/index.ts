c1ae73a792ec6d557222d9665b5389e80c055e33
shaun <beingswu@gmail.com>
2024-08-07 14:19:12 +0800

    update api

--- tasks/ReadImage/index.ts
+++ tasks/ReadImage/index.ts
@@ -9,7 +9,7 @@ export default async function(inputs: Inputs, context: BlockContext): Promise<Ou
   const Sharp = sharp(inputs.image)
   const obj = await Sharp.toBuffer({resolveWithObject: true})
   const base64 = `data:image/png;base64,${obj.data.toString("base64")}`
-  context.sendMessage({type: "image", data: base64})
+  context.preview({type: "image", data: base64})
   return {
     sharp: Sharp,
   }
